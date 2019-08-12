fad4a8203a7e4fbe60883ea49b676171b26f4319
Joe Flateau <joe@joeflateau.net>
2019-08-12 13:06:20 -0400

    Adding myself as a maintainer of video.js
    per #37037

--- types/video.js/index.d.ts
+++ types/video.js/index.d.ts
@@ -8,7 +8,8 @@
 //                 Grzegorz Błaszczyk <https://github.com/gjanblaszczyk>
 //                 Stéphane Roucheray <https://github.com/sroucheray>
 //                 Adam Eisenreich <https://github.com/AkxeOne>
-//                 Mei Qingguang <https://github.com/meikidd>
+//                 Mei Qingguang <https://github.com/meikidd>>
+//                 Joe Flateau <https://github.com/joeflateau>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
